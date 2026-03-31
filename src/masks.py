fa781cced819b7560ae6871d0fc9c97626cd1d8b
добровольский сергей <sepaaa50@gmail.com>
2026-03-31 16:08:52 +0300

    Библиотека logging

--- src/masks.py
+++ src/masks.py
@@ -1,23 +1,57 @@
+import logging
+import os
 from typing import Union
+
+# Автоматический отступ к корню проекта, чтобы не падали тесты
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+
+# Авто-создание папки
+os.makedirs(LOG_DIR, exist_ok=True)
+# Полный путь к файлу
+LOG_FILE = os.path.join(LOG_DIR, "masks.log")
+
+# Логирование.
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/masks.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_mask_card_number(number_card: Union[str, int]) -> str:
     """Функция маскирует номер карты"""
-    # Переводим в строку.
-    number_card_str = str(number_card)
-    # считаем количество введенных цифр и сравниваем с условием.
-    if len(number_card_str) != 16:
-        return "Неполный ввод,такой карты нет."
-    # Выводим результат при помощи срезов
-    return f" {number_card_str[:4]} {number_card_str[4:6]}** **** {number_card_str[12:]}"
+    try:
+        logger.debug("Запускаю функцию маскировки")
+        # Переводим в строку.
+        number_card_str = str(number_card)
+        # считаем количество введенных цифр и сравниваем с условием.
+        if len(number_card_str) != 16:
+            logger.warning("Ввод не корректен")
+            return "Неполный ввод,такой карты нет."
+        # Выводим результат при помощи срезов
+        logger.debug("Корректный ввод")
+        return f" {number_card_str[:4]} {number_card_str[4:6]}** **** {number_card_str[12:]}"
+    except Exception as e:
+        logger.error(f"Ошибка при маскировке: {e}")
+        return "Ошибка данных"
 
 
 def get_mask_account(number_account: Union[str, int]) -> str:
     """Функция маскерует номер счета, оставляя только последние 4 цифры"""
+    try:
 
-    number_account_str = str(number_account)
-    if len(number_account_str) != 20:
-        return "Неправильный ввод."
+        logger.debug("Запускаю фцнкцию проверки счета")
+        number_account_str = str(number_account)
+        logger.debug("Проверяю на длину ввода")
+        if len(number_account_str) != 20:
+            logger.warning("Ввод не корректен")
+            return "Неправильный ввод."
 
-    # Выводим результат.
-    return f" **{number_account_str[-4:]}"
+        # Выводим результат.
+        logger.debug("Корректный ввод")
+        return f" **{number_account_str[-4:]}"
+    except Exception as e:
+        logger.error(f"Ошибка при маскировке: {e}")
+        return "Ошибка данных"
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,35 @@
 import json
+import logging
+
+# Логирование.
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("logs/utils.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def get_transactions(path):
     """Открываем и читаем файл operations.json"""
     try:
+        logger.debug(f"Открываю файл")
         with open(path, encoding="utf-8") as f:
             data = json.load(f)
+
             if isinstance(data, list):  # Проверяем является ли списком
+                logger.debug("Данные успешно прочитаны, это список")
                 return data
             else:
+                logger.warning("Файл прочитан, но данные не являются списком")
                 return []
 
-    except FileNotFoundError:
+    except FileNotFoundError as f:
+        logger.error(f"Произошла ошибка: {f}")
         return []  # Возвращаем пустой список при возникновении ошибки
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as f:
+        logger.error(f"Произошла ошибка: {f}")
         return []
+
+
+print(get_transactions("test.json"))
