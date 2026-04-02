042786806234ad956480496013fafce66a12a6ef
добровольский сергей <sepaaa50@gmail.com>
2026-04-02 13:04:38 +0300

    Работа с CSV- и Excel-файлами

---
+++ src/process_csv.py
@@ -0,0 +1,26 @@
+import csv
+from typing import Any
+
+import pandas as pd
+
+
+def load_csv_data(transactions: str) -> list[dict[str, Any]]:
+    """Функция для считывания csv файла и возврата списка словарей."""
+
+    with open(transactions, encoding="utf-8") as file:  # Открываем файл csv
+        reader = csv.DictReader(file)
+        result = []
+        for row in reader:  # Итерация по файлу csv
+            result.append(row)
+        return result  # Возвращаем список словарей
+
+
+def import_excel_data(transactions_two: str) -> list[dict[str, Any]]:
+    """Функция для считывания Exсel файла"""
+    df = pd.read_excel(transactions_two)
+    result = df.to_dict(orient="records")
+    return result
+
+
+# print(import_excel_data("C:/Users/sepaa/PycharmProjects/test_project/data/transactions_excel.xlsx"))
+# load_csv_data("C:/Users/sepaa/PycharmProjects/test_project/data/transactions.csv")
---
+++ test/test_process_csv.py
@@ -0,0 +1,33 @@
+
+from unittest.mock import mock_open
+from unittest.mock import patch
+
+import pandas as pd
+
+from src.process_csv import import_excel_data
+from src.process_csv import load_csv_data
+
+
+def test_load_csv_data():
+    # Имитируем содержимое CSV файла (заголовки и одна строка)
+    csv_content = "id,amount,currency\n1,100,RUB"
+
+    # Патчим open, чтобы он не лез на диск, а отдавал наш csv_content
+    with patch("builtins.open", mock_open(read_data=csv_content)) as mocked_file:
+        result = load_csv_data("fake_path.csv")
+
+        # Проверяем результат
+        assert result == [{"id": "1", "amount": "100", "currency": "RUB"}]
+        mocked_file.assert_called_once_with("fake_path.csv", encoding="utf-8")
+
+
+@patch("pandas.read_excel")
+def test_import_excel_data(mock_read):
+    # Создаем фейковый DataFrame, который якобы вернул pandas
+    mock_read.return_value = pd.DataFrame([{"id": 2, "amount": 500}])
+
+    result = import_excel_data("fake.xlsx")
+
+    # Проверяем, что функция правильно вызвала pandas и выдала список словарей
+    assert result == [{"id": 2, "amount": 500}]
+    mock_read.assert_called_once_with("fake.xlsx")
